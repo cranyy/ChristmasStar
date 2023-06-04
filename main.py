cfeb4d13ca0136f45daa3b5c703091fd9e4d41b5
cranyy <128929586+cranyy@users.noreply.github.com>
2023-06-04 03:18:48 +0300

    Add files via upload
    Refactoring the main function into smaller modules.

--- main.py
+++ main.py
@@ -2,31 +2,18 @@ import pandas as pd
 import numpy as np
 import yfinance as yf
 import datetime as dt
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.metrics import mean_squared_error
 import multiprocessing as mp
 import platform
 import logging
 from tqdm import tqdm
-import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
-from sklearn.linear_model import LinearRegression
-import joblib
 import os
-from sklearn.metrics import mean_squared_error, r2_score
 import json
-import matplotlib.pyplot as plt
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.feature_selection import RFE, SelectKBest, f_regression
-from sklearn.linear_model import Lasso
-from sklearn.pipeline import Pipeline
-from sklearn.model_selection import GridSearchCV, ParameterGrid
-import hyperfeatey 
+import backtrader as bt
+
+from mls import neural_network_model, random_forest_model, linear_regression_model
 import stoceky
 import plotey
-from hyperfeatey import feature_selection, score_hyperparams, custom_cv_splits
+
 
 
 
@@ -51,157 +38,60 @@ from tensorflow.keras.optimizers import Adam
 from sklearn.preprocessing import StandardScaler
 import keras
 
-def neural_network_model(stock_data, retrain=False):
-    model_file = 'neural_network_model.h5'
-    
-    if os.path.isfile(model_file) and not retrain:
-        model = keras.models.load_model(model_file)
-        print(f"Loaded neural network model from {model_file}")
-    
-    stock_data = stock_data.dropna()
-    selected_features = feature_selection(stock_data, method="SelectKBest", n_features=7)
-    X = stock_data[selected_features]
-    y = stock_data['Close']
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-    
-    # Scale the features
-    scaler = StandardScaler()
-    X_train = scaler.fit_transform(X_train)
-    X_test = scaler.transform(X_test)
 
-    # Create the neural network model
-    model = Sequential()
-    model.add(Dense(64, activation='relu', input_shape=(X_train.shape[1],)))
-    model.add(Dense(32, activation='relu'))
-    model.add(Dense(1))
+import backtrader as bt
+import numpy as np
 
-    model.compile(optimizer=Adam(learning_rate=0.001), loss='mean_squared_error')
+class PredictiveStrategy(bt.Strategy):
+    def __init__(self):
+        self.dataclose = self.datas[0].close
+        self.order = None
+        self.buyprice = None
+        self.buycomm = None
+        self.bar_executed = 0
+        self.orderid = 0
 
-    # Train the model
-    model.fit(X_train, y_train, epochs=75, batch_size=32, verbose=2)
+    def log(self, txt, dt=None):
+        dt = dt or self.datas[0].datetime.date(0)
+        print(f'{dt.isoformat()}, {txt}')
 
-    # Evaluate the model
-    y_pred = model.predict(X_test).flatten()
-    mse = mean_squared_error(y_test, y_pred)
-    r2 = r2_score(y_test, y_pred)
+    def notify_order(self, order):
+        if order.status in [order.Submitted, order.Accepted]:
+            return
 
-    last_365_days = stock_data[X.columns].tail(365)
-    last_365_days_scaled = scaler.transform(last_365_days)
-    future_prices_all_days = model.predict(last_365_days_scaled).flatten()
+        if order.status in [order.Completed]:
+            if order.isbuy():
+                self.log(f'BUY EXECUTED for order #{self.orderid}, Price: {order.executed.price}, Cost: {order.executed.value}, Comm: {order.executed.comm}')
+                self.buyprice = order.executed.price
+                self.buycomm = order.executed.comm
+                self.bar_executed = len(self)
+            else:
+                self.log(f'SELL EXECUTED for order #{self.orderid}, Price: {order.executed.price}, Cost: {order.executed.value}, Comm: {order.executed.comm}, Profit: {order.executed.price - self.buyprice}')
+                self.orderid += 1
 
-    predictions_df = pd.DataFrame(index=range(1, 366), columns=['NN_1d_Close_Price_Prediction'])
-    for i in range(1, 366):
-        predictions_df.loc[i, 'NN_1d_Close_Price_Prediction'] = future_prices_all_days[-i]
+        elif order.status in [order.Canceled, order.Margin, order.Rejected]:
+            self.log('Order Canceled/Margin/Rejected')
 
-    print("Neural Network predictions for every single day in the 365-day window:")
-    for i in range(1, 366):
-        print(f"{i} day(s) ahead: {future_prices_all_days[-i]}")
+        self.order = None
 
-    last_180_days = stock_data[X.columns].tail(180)
-    last_180_days_scaled = scaler.transform(last_180_days)
-    future_prices_6m = model.predict(last_180_days_scaled).flatten()
+    def next(self):
+        self.log(f'Close, {self.dataclose[0]}')
+        if self.order:
+            return
 
-    last_365_days = stock_data[X.columns].tail(365)
-    last_365_days_scaled = scaler.transform(last_365_days)
-    future_prices_1y = model.predict(last_365_days_scaled).flatten()
-
-    last_30_days = stock_data[X.columns].tail(30)
-    last_30_days_scaled = scaler.transform(last_30_days)
-    future_prices = model.predict(last_30_days_scaled).flatten()
-
-    model.save(model_file)
-
-    return mse, r2, future_prices, future_prices_all_days, y_test, y_pred, future_prices_6m, future_prices_1y
+        if not self.position:
+            if self.dataclose[0] < self.dataclose[-1]:
+                if self.dataclose[-1] < self.dataclose[-2]:
+                    self.log(f'BUY CREATE for order #{self.orderid}, {self.dataclose[0]}')
+                    self.order = self.buy()
+        else:
+            if len(self) >= (self.bar_executed + 5):
+                self.log(f'SELL CREATE for order #{self.orderid}, {self.dataclose[0]}')
+                self.order = self.sell()
 
 
 
-def random_forest_model(stock_data, retrain=False):
-    model_file = 'random_forest_model.pkl'
-    
-    if os.path.isfile(model_file) and not retrain:
-        model = joblib.load(model_file)
-        print(f"Loaded random forest model from {model_file}")
-    
-    stock_data = stock_data.dropna()
-    selected_features = feature_selection(stock_data, method="SelectKBest", n_features=7)
-    X = stock_data[selected_features]
-    y = stock_data['Close']
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-    scaler = MinMaxScaler()
-    X_train = scaler.fit_transform(X_train)
-    X_test = scaler.transform(X_test)
 
-    model = RandomForestRegressor(random_state=42)
-    rf_params = {'n_estimators': [50, 100],
-                 'max_depth': [None, 10, 20]}
-    grid_search = GridSearchCV(estimator=model, param_grid=rf_params, scoring='neg_mean_squared_error', cv=custom_cv_splits(5), n_jobs=-1, verbose=2)
-    grid_search.fit(X_train, y_train)
-    best_model = grid_search.best_estimator_
-    print(f"{symbol}: GridSearchCV completed for random forest model")
-    best_model = grid_search.best_estimator_
-
-    y_pred = best_model.predict(X_test)
-    mse = mean_squared_error(y_test, y_pred)
-    r2 = r2_score(y_test, y_pred)
-
-    last_30_days = stock_data[X.columns].tail(30)
-    last_30_days_scaled = scaler.transform(last_30_days)
-    future_prices = best_model.predict(last_30_days_scaled)
-    last_180_days = stock_data[X.columns].tail(180)
-    last_180_days_scaled = scaler.transform(last_180_days)
-    future_prices_6m = best_model.predict(last_180_days_scaled)
-
-    last_365_days = stock_data[X.columns].tail(365)
-    last_365_days_scaled = scaler.transform(last_365_days)
-    future_prices_1y = best_model.predict(last_365_days_scaled)
-
-    joblib.dump(best_model, model_file)
-
-    return mse, r2, future_prices, y_test, y_pred, future_prices_6m, future_prices_1y
-
-
-def linear_regression_model(stock_data, retrain=False):
-    model_file = 'linear_regression_model.pkl'
-    if os.path.isfile(model_file) and not retrain:
-        model = joblib.load(model_file)
-        print(f"Loaded linear regression model from {model_file}")
-    stock_data = stock_data.dropna()
-    selected_features = feature_selection(stock_data, method="SelectKBest", n_features=10)
-    X = stock_data[selected_features]
-    y = stock_data['Close']
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-    
-    # Scale the features
-    scaler = MinMaxScaler()
-    X_train = scaler.fit_transform(X_train)
-    X_test = scaler.transform(X_test)
-    
-    model = Lasso(max_iter=10000)  # Increase the number of iterations
-    model.fit(X_train, y_train)
-    lasso_params = {'model__alpha': [0.001, 0.01, 0.1, 1, 10]}
-    pipeline = Pipeline([('model', model)])
-    grid_search = GridSearchCV(estimator=pipeline, param_grid=lasso_params, scoring='neg_mean_squared_error', cv=custom_cv_splits(5))
-    grid_search.fit(X_train, y_train)
-    best_model = grid_search.best_estimator_
-    y_pred = best_model.predict(X_test)
-    mse = mean_squared_error(y_test, y_pred)
-    r2 = r2_score(y_test, y_pred)
-    last_30_days = stock_data[X.columns].tail(30)
-    last_30_days_scaled = scaler.transform(last_30_days)
-    future_prices = model.predict(last_30_days_scaled).flatten()
-
-    last_180_days = stock_data[X.columns].tail(180)
-    last_180_days_scaled = scaler.transform(last_180_days)
-    future_prices_6m = model.predict(last_180_days_scaled).flatten()
-
-    last_365_days = stock_data[X.columns].tail(365)
-    last_365_days_scaled = scaler.transform(last_365_days)
-    future_prices_1y = model.predict(last_365_days_scaled).flatten()
-
-    joblib.dump(best_model, model_file)
-
-    return mse, r2, future_prices, y_test, y_pred, future_prices_6m, future_prices_1y
-    
 
 
 if __name__ == '__main__':
@@ -265,7 +155,7 @@ if __name__ == '__main__':
 
             
             # Train and evaluate the random forest model
-            rf_mse, rf_r2, rf_future_prices, rf_y_test, rf_y_pred, rf_future_prices_6m, rf_future_prices_1y = random_forest_model(stock_data)
+            rf_mse, rf_r2, rf_future_prices, rf_y_test, rf_y_pred, rf_future_prices_6m, rf_future_prices_1y = random_forest_model(stock_data, symbol)
             print(f"{symbol}: Trained and evaluated random forest model")
             print(f"{symbol}: Random Forest MSE: {rf_mse}")
             print()
@@ -314,13 +204,35 @@ if __name__ == '__main__':
                 predictions_df.loc[0, '6m_Close_Price_Prediction_Best_Model'] = lr_future_prices_6m[-1] if best_model == 'LR' else (rf_future_prices_6m[-1] if best_model == 'RF' else nn_future_prices_6m[-1])
                 predictions_df.loc[0, '1y_Close_Price_Prediction_Best_Model'] = lr_future_prices_1y[-1] if best_model == 'LR' else (rf_future_prices_1y[-1] if best_model == 'RF' else nn_future_prices_1y[-1])
                 
+                    # Create a cerebro entity
+                # Create a cerebro entity
+                cerebro = bt.Cerebro()
+
+                # Add a strategy
+                cerebro.addstrategy(PredictiveStrategy)
+
+                # Load data
+                data = bt.feeds.PandasData(dataname=stock_data)
+                cerebro.adddata(data)
+
+                # Set our desired cash start
+                cerebro.broker.setcash(1000.0)
+
+                # Print out the starting conditions
+                print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
+
+                # Run over everything
+                cerebro.run()
+
+                # Print out the final result
+                print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
             
             # Get today's date and tomorrow's date
 
             today_date_date = stock_data.index[-1].strftime("%Y-%m-%d")
             tomorrow_date = (stock_data.index[-1] + pd.DateOffset(days=1)).strftime("%Y-%m-%d")
             
-            plotey.save_plot(stock_data, y_test, y_pred, rf_y_pred, symbol, 'Linear Regression and Random Forest', tomorrow_price, future_prices_7d, future_prices_1m, rf_future_prices, nn_future_prices, nn_y_test, nn_y_pred, rf_future_prices_6m, rf_future_prices_1y, nn_future_prices_6m, nn_future_prices_1y, nn_future_prices_all_days)
+            #plotey.save_plot(stock_data, y_test, y_pred, rf_y_pred, symbol, 'Linear Regression and Random Forest', tomorrow_price, future_prices_7d, future_prices_1m, rf_future_prices, nn_future_prices, nn_y_test, nn_y_pred, rf_future_prices_6m, rf_future_prices_1y, nn_future_prices_6m, nn_future_prices_1y, nn_future_prices_all_days)
 
             # Create a new DataFrame for the stock's predictions
             predictions_df = pd.DataFrame({
@@ -370,3 +282,6 @@ if __name__ == '__main__':
     csv_filename = plotey.generate_timestamped_filename("predictions", "csv")
     combined_predictions_df.to_csv(csv_filename, index=False)
     print(f"Saved combined predictions to {csv_filename}")
+
+
+    
